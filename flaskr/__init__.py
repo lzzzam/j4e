b4f9dc0e71c5d9bad795593e2f132e7e885d266c
Luca Zambuto <Zambuto.external@infineon.com>
2023-01-30 14:58:50 +0100

    added views to support login and logout

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask
 
-def printName():
-    print(__name__)
 
 def create_app(test_config=None):
     # create and configure the app
@@ -32,4 +30,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
     return app
