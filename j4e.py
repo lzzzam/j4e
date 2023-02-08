ee4a6905027b3290223678fb1107b8e9382f92d4
Luca Zambuto <Zambuto.external@infineon.com>
2023-02-08 17:35:14 +0100

    Added email box at the bottom with Cookie to remember box state

--- j4e.py
+++ j4e.py
@@ -1,13 +1,21 @@
 import os
 import db
 from flask import Flask
-from flask import Blueprint, request, flash, g, redirect, render_template, request, session, url_for
+from flask import Blueprint, request, flash, g, redirect, render_template, request, session, url_for, make_response
 
+jobs = [{"title" : "Job#1", "company" : "company#1", "place" : "place#1"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#3", "company" : "company#3", "place" : "place#3"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
+        {"title" : "Job#2", "company" : "company#2", "place" : "place#2"}]
 
 # create and configure the app
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_mapping(
-    SECRET_KEY="dev",
     DATABASE=os.path.join(app.instance_path, "jobs.sqlite"),
 )
 
@@ -15,24 +23,29 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    jobs = [{"title" : "Job#1", "company" : "company#1", "place" : "place#1"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#3", "company" : "company#3", "place" : "place#3"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"},
-            {"title" : "Job#2", "company" : "company#2", "place" : "place#2"}]
-
-    context = {"jobs" : jobs}
+    if (request.cookies.get('subscribed-to-newsletter') == "1") | (request.cookies.get('hidden-subscribe-box') == "1"):
+        showEmailBox = False
+    else:
+        showEmailBox = True
+        
+    context = {"jobs" : jobs, "showEmailBox" : showEmailBox}
     return render_template('index.html.j2', context=context)
 
 
 @app.route('/form', methods=('GET', 'POST'))
 def form():
-    #print(request.form['email'])
-    return redirect('/')
+    print(request.form['email'])
+    resp = make_response('', 204)
+    resp.set_cookie(key='subscribed-to-newsletter', value = "1")
+    return resp
+
+@app.route('/close_form')
+def close_form():
+    print('close form')
+    resp = make_response('', 204)
+    resp.set_cookie(key='hidden-subscribe-box', value = "1", max_age=60)
+    return resp
+    
 
 with app.test_request_context():
     print(url_for('index'))
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,13 @@
 const emailbox = document.getElementById('emailbox');
+const email_form = document.getElementById('email-form');
+const close_form = document.getElementById('close_form');
+const thank_message = document.getElementById('thank-message');
 
-emailbox.addEventListener('click', (event) => {
+close_form.addEventListener('click', (event) => {
     emailbox.remove();
+});
+
+email_form.addEventListener('submit', (event) => {
+    thank_message.style.display = 'block'
+    email_form.style.display = 'none'
 });
