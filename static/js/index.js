d07b5dcc0137f8de923dec3858f094d9a1c8eca0
Luca Zambuto <Zambuto.external@infineon.com>
2023-02-08 18:13:02 +0100

    fixed small error in index.js

--- static/js/index.js
+++ static/js/index.js
@@ -3,11 +3,17 @@ const email_form = document.getElementById('email-form');
 const close_form = document.getElementById('close_form');
 const thank_message = document.getElementById('thank-message');
 
-close_form.addEventListener('click', (event) => {
-    emailbox.remove();
-});
+if(close_form)
+{
+    close_form.addEventListener('click', (event) => {
+        emailbox.remove();
+    });    
+}
 
-email_form.addEventListener('submit', (event) => {
-    thank_message.style.display = 'block'
-    email_form.style.display = 'none'
-});
+if(close_form)
+{
+    email_form.addEventListener('submit', (event) => {
+        thank_message.style.display = 'block'
+        email_form.style.display = 'none'
+    });
+}
