d07bcda208d7a78dc18d9a039973fce8e25728ec
lzzzam <luca.zambuto1@gmail.com>
2023-02-26 13:54:45 +0100

    General improvement to user interface

--- static/js/search.js
+++ static/js/search.js
@@ -1,8 +1,33 @@
+import Typed from './typed.js';
+
+// auto type city in the header
+const newCity = new Typed('#header-city', {
+    strings: [
+        'Munich', 'Vienna', 'Paris',
+        'Warsaw', 'Berlin', 'Rome',
+        'Madrid', 'Zagreb', 'Athens',
+        'Lisbon', 'Milan'
+    ],
+    // typing-in speed. default value is one
+    typeSpeed: 1,
+    // typing-out speed. default value is one
+    backSpeed: 1,
+    // occur once or forever. default value is false
+    loop: true,
+    // color of insertion point. default value is black
+    IPColor: "white",
+});
+// Initialize Typing Effect
+newCity.start();
+
 function searchJob() {
     jobTitle = document.getElementById("title")
     jobCity = document.getElementById("city")
     jobSeniority = document.getElementById("seniority")
 }
+$("#search-text").click(function () {
+    $(this).css('border-width', '3px');
+});
 
 // toogle description when click on job card
 $('#job-list').on('click', '.jobcard', function () {
@@ -17,4 +42,14 @@ $('#job-list').on('mouseover', '.jobcard', function () {
 // restore job card color when mouse is out
 $('#job-list').on('mouseout', '.jobcard', function () {
     $(this).css('background-color', 'transparent');
+});
+
+// highlight job board logo when mouse is over
+$('.clickable').mouseover(function () {
+    $(this).css('opacity', '0.6');
+});
+
+// restore job board logo when mouse is out
+$('.clickable').mouseout(function () {
+    $(this).css('opacity', '1.0');
 });
