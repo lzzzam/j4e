b339c0f46b9db7dccb8c8d66a8959aa9848a237b
lzzzam <luca.zambuto1@gmail.com>
2023-03-10 21:46:45 +0100

    Added command to log git branch as graph

--- tasks.py
+++ tasks.py
@@ -1,5 +1,9 @@
 from invoke import task
 
 @task
-def run(c):
+def startApp(c):
     c.run("flask --app j4e --debug run --host=0.0.0.0")
+    
+@task
+def gitLog(c):
+    c.run("git log --all --decorate --oneline --graph")
